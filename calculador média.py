696a20f8dc53dc88ca02154d41aaf842889059fd
Ryan Dias <161870754+ryxvdz@users.noreply.github.com>
2024-03-01 14:20:57 -0300

    Add files via upload

---
+++ calculador média.py
@@ -0,0 +1,13 @@
+n1 = float(input ("Digite a primeira nota:"))
+n2 = float(input ("Digite a segunda nota:"))
+n3 = float(input ("Digite a terceira nota:"))
+n4 = float(input ("Digite a quarta nota:"))
+media = (n1+ n2+ n3+ n4) / 4
+print("Sua media foi", media)
+if media >=6:
+     
+    print("Você foi aprovado!")
+else: 
+    print("Você não foi aprovado :(")
+
+
